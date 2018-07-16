b178ebf77d157898f6ce0551266367f79a0e4849
Said <saidllamas14@gmail.com>
2018-07-16 17:39:41 -0500

    Init

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/parciales');
+
+// Express HBS engine
+app.set('view engine', 'hbs');
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {
+        nombre: 'said'
+    });
+
+});
+
+app.get('/about', function(req, res) {
+
+    res.render('about');
+
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
